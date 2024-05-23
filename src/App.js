d7541523ea4d9959948d1fccbdd93ff72001c418
Alexsey Sinyakin <lexavandal@mail.ru>
2024-05-23 21:46:23 +0300

    not jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { createElement } from "react";
 
 export const App = () => {
-    const yers = new Date(); // императивный стиль
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload 2.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <span> {yers.getFullYear()} </span>    {/* императивный  */}
-            </header>
-        </div>
+    const yers = new Date();
+    return createElement(
+        "div",
+        { className: "App" },
+        React.createElement(
+            "header",
+            { className: "App-header" },
+            React.createElement("img", {
+                className: "App-logo",
+                src: logo,
+                alt: "logo",
+            }),
+            React.createElement(
+                "p",
+                null,
+                "Edit",
+                React.createElement("code", null, " src/App.js"),
+                "and save to reload 2.",
+            ),
+            React.createElement(
+                "a",
+                {
+                    className: "App-link",
+                    href: "https://reactjs.org",
+                    target: "_blank",
+                    ral: "noopener noreferrer",
+                },
+                "Learn React",
+            ),
+            React.createElement("span", null, `${yers.getFullYear()}`),
+        ),
     );
 };
-// остальной код написан в декларативном стиле
