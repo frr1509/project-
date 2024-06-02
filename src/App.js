8ebeec20e3d8857b4f5b0ff14a10e1d65a5f9189
Alexsey Sinyakin <lexavandal@mail.ru>
2024-06-02 21:36:45 +0300

    zadanie1

--- src/App.js
+++ src/App.js
@@ -1,38 +1,72 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React, { createElement } from "react";
+import { useState } from "react";
+import styles from "./App.module.css";
 
 export const App = () => {
-    const yers = new Date();
-    return createElement(
-        "div",
-        { className: "App" },
-        React.createElement(
-            "header",
-            { className: "App-header" },
-            React.createElement("img", {
-                className: "App-logo",
-                src: logo,
-                alt: "logo",
-            }),
-            React.createElement(
-                "p",
-                null,
-                "Edit",
-                React.createElement("code", null, " src/App.js"),
-                "and save to reload 2.",
-            ),
-            React.createElement(
-                "a",
-                {
-                    className: "App-link",
-                    href: "https://reactjs.org",
-                    target: "_blank",
-                    ral: "noopener noreferrer",
-                },
-                "Learn React",
-            ),
-            React.createElement("span", null, `${yers.getFullYear()}`),
-        ),
+    const [value, setValue] = useState("");
+    const [list, setList] = useState([]);
+    const [error, setError] = useState("");
+
+    let isValueVaild = value.length < 3 ? false : true;
+
+    const onInputButtonClick = () => {
+        isValueVaild = true;
+        const promptValue = prompt();
+        if (promptValue.length < 3) {
+            setError("Введенное значение должно содержать минимум 3 символа");
+        } else {
+            setValue(promptValue);
+            setError("");
+        }
+    };
+
+    const onAddButtonClick = () => {
+        if (isValueVaild) {
+            setValue("");
+            setError("");
+            let updateList = [
+                ...list,
+                { id: Date.now(), value, date: new Date() },
+            ];
+            setList(updateList);
+        }
+    };
+    return (
+        <div className={styles.app}>
+            <h1 className={styles["page-heading"]}>Ввод значения</h1>
+            <p className={styles["no-margin-text"]}>
+                Текущее значение <code>value</code>: "
+                <output className={styles["current-value"]}>{value}</output>"
+            </p>
+            {error !== "" && <div className={styles.error}>{error}</div>}
+            <div className={styles["buttons-container"]}>
+                <button className={styles.button} onClick={onInputButtonClick}>
+                    Ввести новое
+                </button>
+                <button
+                    className={styles.button}
+                    disabled={!isValueVaild}
+                    onClick={onAddButtonClick}
+                >
+                    Добавить в список
+                </button>
+            </div>
+            <div className={styles["list-container"]}>
+                <h2 className={styles["list-heading"]}>Список:</h2>
+                {list.length === 0 ? (
+                    <p className={styles["no-margin-text"]}>
+                        Нет добавленных элементов
+                    </p>
+                ) : (
+                    <ul className={styles.list}>
+                        {list.map((item) => (
+                            <li className={styles["list-item"]} key={item.id}>
+                                {item.value} {item.date.toLocaleDateString()}{" "}
+                                {item.date.toLocaleTimeString()}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </div>
     );
 };
